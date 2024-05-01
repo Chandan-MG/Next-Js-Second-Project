cc36c7f27b15d3987988cfe9d9d19f0a8354b7a1
Chandan-MG <chandanmg.98@gmail.com>
2024-05-02 01:30:27 +0530

    Prerendering the powers of Next JS

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import MeetupList from "../components/meetups/MeetupList";
+import { useEffect, useState } from "react";
 
 const DUMMY_ITEM = [
     {
@@ -14,12 +15,23 @@ const DUMMY_ITEM = [
         image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/S%C3%A3o_Paulo_City.jpg/1280px-S%C3%A3o_Paulo_City.jpg',
         address: 'Some address 3, 55 some city',
         description: 'This is a second meetup'
+    },
+    {
+        id: 'm3',
+        title: 'A third meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/S%C3%A3o_Paulo_City.jpg/1280px-S%C3%A3o_Paulo_City.jpg',
+        address: 'Some address 3, 55 some city',
+        description: 'This is a third meetup'
     }
 ]
 
 function HomePage(){
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+    useEffect(()=>{
+        setLoadedMeetups(DUMMY_ITEM);
+    },[])
     return(
-        <MeetupList meetups={DUMMY_ITEM} />
+        <MeetupList meetups={loadedMeetups} />
     )
 }
 
