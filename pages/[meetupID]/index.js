ac7e69bb9b45ccc1f2035cd4cd90d4e556abc1c2
Chandan-MG <chandanmg.98@gmail.com>
2024-05-03 00:42:09 +0530

    Making the Details page work properly

--- pages/[meetupID]/index.js
+++ pages/[meetupID]/index.js
@@ -1,7 +1,8 @@
 import { Fragment } from "react";
 import classes from './index.module.css';
+import {MongoClient} from 'mongodb';
 
-function MeetUpDetails(){
+function MeetUpDetails(props){
     return(
         <section className={classes.detail}>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/City-of-the-future.jpg/1280px-City-of-the-future.jpg" alt="A first meetup" />
@@ -12,4 +13,42 @@ function MeetUpDetails(){
     )
 }
 
+export async function getStaticPaths(){
+    const client = await MongoClient.connect('mongodb://localhost:27017/meetupsdb');
+    const db = client.db;
+    const meetupsCollection = db.collection('meetups');
+
+    const meetups = await meetupsCollection.find({}, {_id: 1}).toArray();
+    client.close();
+
+    return{
+        fallback: false,
+        paths:  meetups.map((m) => ({params:{ meetupId : m._id.toString()}}))
+    }
+}
+
+
+export async function getStaticProps(context){
+    const meetupId = context.params.meetupId;
+
+    const client = await MongoClient.connect('mongodb://localhost:27017/meetupsdb');
+    const db = client.db;
+    const meetupsCollection = db.collection('meetups');
+
+    const selectedMeetup = await meetupsCollection.findOne({_id: ObjectId(meetupId)})
+
+    client.close();
+
+    return{
+        props: {
+            meetupdata: {
+                id: selectedMeetup._id.toString(),
+                title: selectedMeetup.title,
+                image: selectedMeetup.image,
+                address: selectedMeetup.address,
+                description: selectedMeetup.description,
+            }
+        }
+    }
+}
 export default MeetUpDetails;
